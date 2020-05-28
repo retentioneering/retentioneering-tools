7f76cd2df5bfdae5c6c42410f28a30871d32f1d0
tokedo <zaytsev.anatoly@gmail.com>
2020-05-28 14:50:15 -0700

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='retentioneering',
-    version='1.0.8.5',
+    version='2.0.0.1',
     license='Mozilla Public License',
     description='Product analytics and marketing optimization framework based on deep user trajectories analysis',
     long_description="""

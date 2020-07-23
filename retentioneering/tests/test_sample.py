138e79d8f1e739fa871d6cbbcef7e628bc7cd774
tokedo <zaytsev.anatoly@gmail.com>
2020-07-22 22:30:24 -0700

    add sample test

---
+++ retentioneering/tests/test_sample.py
@@ -0,0 +1,24 @@
+from retentioneering import init_config
+from retentioneering import datasets
+
+data = datasets.load_simple_shop()
+
+
+def test_import():
+    assert len(data) == 35357
+
+
+init_config(
+    experiments_folder='experiments', # folder for saving experiment results: graph visualization, heatmaps and etc.
+    index_col='client_id', # column by which we split users / sessions / whatever
+    event_col='event', # column that describes event
+    event_time_col='timestamp', # column that describes timestamp of event
+    positive_target_event='passed', # name of positive target event
+    negative_target_event='lost', # name of negative target event
+    pos_target_definition={ # how to define positive event, e.g. empty means that add passed for whom was not 'lost'
+        'event_list': ['payment_done']
+    },
+    neg_target_definition={}
+)
+
+data = data.retention.prepare()

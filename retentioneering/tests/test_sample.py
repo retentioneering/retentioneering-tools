c525a0589778938524c672f58e64fd946c2f5449
tokedo <zaytsev.anatoly@gmail.com>
2020-07-22 22:58:26 -0700

    add sample test for retention.get_edgelist function

--- retentioneering/tests/test_sample.py
+++ retentioneering/tests/test_sample.py
@@ -4,10 +4,7 @@ from retentioneering import datasets
 data = datasets.load_simple_shop()
 
 
-def test_import():
-    assert len(data) == 35357
-
-
+# setup init_config and apply retention.prepare()
 init_config(
     experiments_folder='experiments', # folder for saving experiment results: graph visualization, heatmaps and etc.
     index_col='client_id', # column by which we split users / sessions / whatever
@@ -20,5 +17,25 @@ init_config(
     },
     neg_target_definition={}
 )
-
 data = data.retention.prepare()
+
+# Test if sample dataset imported correctly
+def test_import():
+    assert len(data) == 35357
+
+
+#test for retention.get_edgelist function:
+def test_get_edgelist():
+    #obtain edgelist sorted vector
+    test = data.retention.get_shift()
+    test['bi_gram'] = test['event'] + '~~~' + test['next_event']
+    res = test['bi_gram'].value_counts().values
+
+    #use RETE to obtain edgelist
+    edgelist = data.retention.get_edgelist(norm_type=None,
+                                           weight_col=None)
+    edgelist.sort_values('edge_weight', ascending=False, inplace=True)
+    edgelist['edge_weight'].values
+
+    #check equality
+    assert all(res == edgelist['edge_weight'].values)

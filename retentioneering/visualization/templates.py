dc7e4eece7502bd1100f75ac8fb0666dce794386
Alexey Avramenko <2822698@gmail.com>
2020-12-03 19:47:18 +0700

    d3

--- retentioneering/visualization/templates.py
+++ retentioneering/visualization/templates.py
@@ -32,7 +32,7 @@ __TEMPLATE__ = """
   <meta charset="UTF-8">
   <title>Graph Editor</title>
   <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-  <script src="https://d3js.org/d3.v5.min.js"></script>
+  <script src="https://api.retentioneering.com/files/d3.v4.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">

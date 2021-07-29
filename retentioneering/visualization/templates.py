f4bc691a0e4f75409c420d2d356e42783838bfbc
Alexey Avramenko <2822698@gmail.com>
2021-07-29 23:19:38 +0700

    fix layout dump

--- retentioneering/visualization/templates.py
+++ retentioneering/visualization/templates.py
@@ -36,7 +36,7 @@ __TEMPLATE__ = """
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <script src="https://static.server.retentioneering.com/viztools/draw-graph.min.js" type="text/javascript"></script>
+  <script src="http://localhost:8080/draw-graph.min.js" type="text/javascript"></script>
   <style type="text/css">
       .download {{
         display: flex;
@@ -328,7 +328,7 @@ __TEMPLATE__ = """
   </main>
   <script type="text/javascript">
     updateLinkThresholdText({thresh}*{scale});
-    initialize({nodes}, {node_params}, {links});
+    initialize({nodes}, {node_params}, {links}, {layout_dump});
 
     if (!{show_percent}) {{
       $('.percent-checkbox').hide();

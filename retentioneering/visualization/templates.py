ed51b49f4a0121cbc9ac7607d1b474546989f138
Maxim Godzi <mzx@mail.ru>
2021-12-04 00:40:12 +0300

    Update templates.py

--- retentioneering/visualization/templates.py
+++ retentioneering/visualization/templates.py
@@ -8,7 +8,7 @@ __VEGA_TEMPLATE__ = """
 <html>
 <head>
   <meta charset="UTF-8"
-  <script src="https://api.retentioneering.com/files/d3.v4.min.js?a={func_name}"></script>
+  <script src="https://static.server.retentioneering.com/files/d3.v4.min.js?a={func_name}"></script>
   <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/vega@5"></script>
   <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/vega-lite@4"></script>
   <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/vega-embed@3.4"></script>
@@ -32,7 +32,7 @@ __TEMPLATE__ = """
   <meta charset="UTF-8">
   <title>Graph Editor</title>
   <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-  <script src="https://api.retentioneering.com/files/d3.v4.min.js"></script>
+  <script src="https://static.server.retentioneering.com/files/d3.v4.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -399,7 +399,7 @@ __OLD_TEMPLATE__ = """
                 }}
 </style>
 <body>
-<script src="https://api.retentioneering.com/files/d3.v4.min.js"></script>
+<script src="https://static.server.retentioneering.com/files/d3.v4.min.js"></script>
 <div>
   <input type="checkbox" class="checkbox" value="weighted"><label> Show weights </label>
 </div>

5ada010dedf3b66df9452a2cfe9d465a206b99cc
Alexey Avramenko <2822698@gmail.com>
2022-07-06 19:34:28 +0300

    lib fix

--- src/graph/factory_lib.py
+++ src/graph/factory_lib.py
@@ -1,8 +1,35 @@
-from typing import Callable, List, Optional, Tuple, cast
+from abc import abstractmethod
+from typing import Any, Callable, List, Optional, Tuple, TypedDict, cast
+from xmlrpc.client import Boolean
 from eventstream.eventstream import Eventstream
 from dataclasses import dataclass, field
 
 EventsFactory = Callable[[Eventstream], Eventstream]
+
+class AvailableType:
+  name: str
+  @abstractmethod
+  def is_valid(self, value: Any):
+    return True
+
+class String(AvailableType):
+  name: str = "string"
+  def is_vaid(self, value: str):
+    return type(value) == "str"
+   
+class Enum(AvailableType):
+  name: str = "enum"
+  values: List[str]
+
+  def __init__(self, values: List[str]):
+    super().__init__()
+    self.values = values
+
+  def is_valid(self, value: str):
+    return value in self.values
+    
+  
+
 
 @dataclass
 class DataProcessor():

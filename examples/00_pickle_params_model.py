4cc7b49d835b0fef319a27195be0b7a271027b46
Vladimir Makhanov <makhanov.vladimir@gmail.com>
2022-08-09 15:30:22 +0500

    add example of pickling params model

---
+++ examples/00_pickle_params_model.py
@@ -0,0 +1,42 @@
+import pickle
+from _pickle import PicklingError
+from typing import Callable
+
+from src.params_model import ReteParamsModel
+
+
+class Test(ReteParamsModel):
+    a: int
+    b: str
+    c: Callable
+
+
+def f(x):
+    return x > 10
+
+
+data1 = {
+    'a': 1,
+    'b': 'asd',
+    'c': lambda x: x > 10
+}
+
+data2 = {
+    'a': 1,
+    'b': 'asd',
+    'c': f
+}
+
+if __name__ == '__main__':
+
+    # Cant pickle lambdas
+    try:
+        t = Test(**data1)
+        s = pickle.dumps(t, protocol=5)
+    except pickle.PicklingError as e:
+        print('Dont allow lambdas in pickle')
+    
+    # Picke with function-name
+    t = Test(**data2)
+    ts = pickle.dumps(t, protocol=5)
+    print(len(ts))

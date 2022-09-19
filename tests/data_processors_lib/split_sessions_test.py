b7e56de0230bae382477d4d561a958d3ca3a5ce1
v.kukushkin <kukushkin.v@gmail.com>
2022-09-19 14:05:40 +0200

    PLAT-131

--- tests/data_processors_lib/split_sessions_test.py
+++ tests/data_processors_lib/split_sessions_test.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.data_processors_lib.rete import SplitSessions, SplitSessionsParams
 from src.eventstream.eventstream import Eventstream
 from src.eventstream.schema import EventstreamSchema, RawDataSchema
+from src.graph.p_graph import PGraph, EventsNode
 
 
 class TestSplitSessions:
@@ -49,3 +50,105 @@ class TestSplitSessions:
         events_names: list[str] = result_df[result.schema.event_name].to_list()
         # @TODO: add correct test after example of needed work. Vladimir Makhanov
         assert True
+
+    def test_split_sesssion_2(self):
+        source_df = pd.DataFrame(
+            [
+                [111, 'event1', '2022-01-01 00:00:00'],
+                [111, 'event2', '2022-01-01 00:01:00'],
+                [111, 'event3', '2022-01-01 00:33:00'],
+                [111, 'event4', '2022-01-01 00:34:00'],
+                [222, 'event1', '2022-01-01 00:30:00'],
+                [222, 'event2', '2022-01-01 00:31:00'],
+                [222, 'event3', '2022-01-01 01:01:00'],
+                [333, 'event1', '2022-01-01 01:00:00'],
+                [333, 'event2', '2022-01-01 01:01:00'],
+                [333, 'event3', '2022-01-01 01:32:00'],
+                [333, 'event4', '2022-01-01 01:33:00']
+            ], columns=['user_id', 'event', 'timestamp']
+        )
+
+        correct_result_columns = ['user_id', 'event_name', 'event_type', 'event_timestamp', 'session_id']
+        correct_result_1 = pd.DataFrame(
+            [
+                [111, 'session_start', 'session_start', '2022-01-01 00:00:00', '111_1'],
+                [111, 'event1', 'raw', '2022-01-01 00:00:00', '111_1'],
+                [111, 'event2', 'raw', '2022-01-01 00:01:00', '111_1'],
+                [111, 'session_end', 'session_end', '2022-01-01 00:01:00', '111_1'],
+                [111, 'session_start', 'session_start', '2022-01-01 00:33:00', '111_2'],
+                [111, 'event3', 'raw', '2022-01-01 00:33:00', '111_2'],
+                [111, 'event4', 'raw', '2022-01-01 00:34:00', '111_2'],
+                [111, 'session_end', 'session_end', '2022-01-01 00:34:00', '111_2'],
+                [222, 'session_start', 'session_start', '2022-01-01 00:30:00', '222_1'],
+                [222, 'event1', 'raw', '2022-01-01 00:30:00', '222_1'],
+                [222, 'event2', 'raw', '2022-01-01 00:31:00', '222_1'],
+                [222, 'event3', 'raw', '2022-01-01 01:01:00', '222_1'],
+                [222, 'session_end', 'session_end', '2022-01-01 01:01:00', '222_1'],
+                [333, 'session_start', 'session_start', '2022-01-01 01:00:00', '333_1'],
+                [333, 'event1', 'raw', '2022-01-01 01:00:00', '333_1'],
+                [333, 'event2', 'raw', '2022-01-01 01:01:00', '333_1'],
+                [333, 'session_end', 'session_end', '2022-01-01 01:01:00', '333_1'],
+                [333, 'session_start', 'session_start', '2022-01-01 01:32:00', '333_2'],
+                [333, 'event3', 'raw', '2022-01-01 01:32:00', '333_2'],
+                [333, 'event4', 'raw', '2022-01-01 01:33:00', '333_2'],
+                [333, 'session_end', 'session_end', '2022-01-01 01:33:00', '333_2'],
+            ], columns=correct_result_columns
+        )
+
+        correct_result_2 = pd.DataFrame(
+            [
+                [111, 'session_start', 'session_start', '2022-01-01 00:00:00', '111_1'],
+                [111, 'session_start_truncated', 'session_start_truncated', '2022-01-01 00:00:00', '111_1'],
+                [111, 'event1', 'raw', '2022-01-01 00:00:00', '111_1'],
+                [111, 'event2', 'raw', '2022-01-01 00:01:00', '111_1'],
+                [111, 'session_end', 'session_end', '2022-01-01 00:01:00', '111_1'],
+                [111, 'session_start', 'session_start', '2022-01-01 00:33:00', '111_2'],
+                [111, 'event3', 'raw', '2022-01-01 00:33:00', '111_2'],
+                [111, 'event4', 'raw', '2022-01-01 00:34:00', '111_2'],
+                [111, 'session_end', 'session_end', '2022-01-01 00:34:00', '111_2'],
+                [222, 'session_start', 'session_start', '2022-01-01 00:30:00', '222_1'],
+                [222, 'event1', 'raw', '2022-01-01 00:30:00', '222_1'],
+                [222, 'event2', 'raw', '2022-01-01 00:31:00', '222_1'],
+                [222, 'event3', 'raw', '2022-01-01 01:01:00', '222_1'],
+                [222, 'session_end', 'session_end', '2022-01-01 01:01:00', '222_1'],
+                [333, 'session_start', 'session_start', '2022-01-01 01:00:00', '333_1'],
+                [333, 'event1', 'raw', '2022-01-01 01:00:00', '333_1'],
+                [333, 'event2', 'raw', '2022-01-01 01:01:00', '333_1'],
+                [333, 'session_end', 'session_end', '2022-01-01 01:01:00', '333_1'],
+                [333, 'session_start', 'session_start', '2022-01-01 01:32:00', '333_2'],
+                [333, 'event3', 'raw', '2022-01-01 01:32:00', '333_2'],
+                [333, 'event4', 'raw', '2022-01-01 01:33:00', '333_2'],
+                [333, 'session_end_truncated', 'session_end_truncated', '2022-01-01 01:33:00', '333_2'],
+                [333, 'session_end', 'session_end', '2022-01-01 01:33:00', '333_2']
+            ], columns=correct_result_columns
+        )
+
+        stream = Eventstream(
+            raw_data_schema=RawDataSchema(
+                event_name='event', event_timestamp='timestamp', user_id='user_id'
+            ),
+            raw_data=source_df,
+            schema=EventstreamSchema(),
+        )
+
+        # основной тест
+        graph = PGraph(source_stream=stream)
+        params = SplitSessionsParams(session_cutoff=(30, 'm'), session_col='session_id')
+        splitted_sessions = EventsNode(SplitSessions(params=params))
+        graph.add_node(node=splitted_sessions, parents=[graph.root])
+        res = graph.combine(node=splitted_sessions).to_dataframe()[correct_result_columns]\
+            .sort_values(['user_id', 'event_timestamp'])\
+            .reset_index(drop=True)
+
+        assert res.compare(correct_result_1).shape == (0, 0)
+
+        # проверка mark_truncated
+        graph = PGraph(source_stream=stream)
+        params = SplitSessionsParams(session_cutoff=(30, 'm'), session_col='session_id', mark_truncated=True)
+        splitted_sessions = EventsNode(SplitSessions(params=params))
+        graph.add_node(node=splitted_sessions, parents=[graph.root])
+        res = graph.combine(node=splitted_sessions).to_dataframe()[correct_result_columns]\
+            .sort_values(['user_id', 'event_timestamp'])\
+            .reset_index(drop=True)
+
+        assert res.compare(correct_result_2).shape == (0, 0)

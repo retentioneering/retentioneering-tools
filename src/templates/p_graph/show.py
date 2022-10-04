0adc559d05c197b25c5404cb7be496d4263ca0da
Vladimir Makhanov <makhanov.vladimir@gmail.com>
2022-10-04 11:38:20 +0500

    fix path

--- src/templates/p_graph/show.py
+++ src/templates/p_graph/show.py
@@ -6,7 +6,7 @@ class PGraphRenderer:
     __environment: Environment
 
     def __init__(self) -> None:
-        self.__environment = Environment(loader=FileSystemLoader("./src/templates/"))
+        self.__environment = Environment(loader=FileSystemLoader("../src/templates"))
         self.__template = self.__environment.get_template("p_graph/p_graph.html")
 
     def show(self, server_id: str, env: str) -> str:

48c16964965b94e92d45692347c497e3cc8e8420
Daria Panina <paninad.a@gmail.com>
2022-10-05 19:05:05 +0200

    add new data processor delete_users_by_path_length

--- src/data_processors_lib/rete/__init__.py
+++ src/data_processors_lib/rete/__init__.py
@@ -1,6 +1,10 @@
 from .collapse_loops import CollapseLoops, CollapseLoopsParams
 from .cut_path_after_event import CutPathAfterEvent, CutPathAfterEventParams
 from .cut_path_before_event import CutPathBeforeEvent, CutPathBeforeEventParams
+from .delete_users_by_path_length import (
+    DeleteUsersByPathLength,
+    DeleteUsersByPathLengthParams,
+)
 from .lost_users import LostUsersEvents, LostUsersParams
 from .negative_target import NegativeTarget, NegativeTargetParams
 from .new_users import NewUsersEvents, NewUsersParams
@@ -8,4 +12,3 @@ from .positive_target import PositiveTarget, PositiveTargetParams
 from .split_sessions import SplitSessions, SplitSessionsParams
 from .start_end_events import StartEndEvents, StartEndEventsParams
 from .truncate_path import TruncatePath, TruncatePathParams
-from .truncated_events import TruncatedEvents, TruncatedParams

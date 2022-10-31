428e64d7d31fba7db071632ddfd224e66340e569
zoidberg <zoidbergx@tlen.pl>
2022-10-31 13:48:35 +0300

    refactor delete_users_by_path_length test

--- tests/data_processors_lib/delete_users_by_path_length_test.py
+++ tests/data_processors_lib/delete_users_by_path_length_test.py
@@ -2,46 +2,63 @@ from __future__ import annotations
 
 import pandas as pd
 
+from src.eventstream.schema import RawDataSchema
 from src.data_processors_lib.rete import (
     DeleteUsersByPathLength,
     DeleteUsersByPathLengthParams,
 )
-from src.eventstream.eventstream import Eventstream
-from src.eventstream.schema import EventstreamSchema, RawDataSchema
-from src.graph.p_graph import EventsNode, PGraph
+from tests.data_processors_lib.common import (
+    apply_processor,
+    apply_processor_with_graph,
+)
 
 
 class TestDeleteUsersByPathLength:
-    def test_delete_users_by_path_length_apply__by_event_4(self):
-        source_df = pd.DataFrame(
-            [
-                [1, "start", "start", "2022-01-01 00:01:00"],
-                [1, "event1", "raw", "2022-01-01 00:01:00"],
-                [1, "event2", "raw", "2022-01-01 00:01:02"],
-                [1, "event1", "raw", "2022-01-01 00:02:00"],
-                [1, "event1", "raw", "2022-01-01 00:03:00"],
-                [1, "event1", "synthetic", "2022-01-01 00:03:00"],
-                [1, "session_start", "session_start", "2022-01-01 00:03:30"],
-                [1, "event3", "raw", "2022-01-01 00:03:30"],
-                [1, "event3_synthetic", "synthetic", "2022-01-01 00:03:30"],
-                [1, "event1", "raw", "2022-01-01 00:04:00"],
-                [1, "event3", "raw", "2022-01-01 00:04:30"],
-                [1, "event1", "raw", "2022-01-01 00:05:00"],
-                [2, "event1", "raw", "2022-01-02 00:00:00"],
-                [2, "event3", "raw", "2022-01-02 00:00:05"],
-                [2, "event2", "raw", "2022-01-02 00:01:05"],
-                [2, "end", "end", "2022-01-02 00:01:05"],
-                [3, "event1", "raw", "2022-01-02 00:01:10"],
-                [3, "event1", "raw", "2022-01-02 00:02:05"],
-                [3, "event4", "raw", "2022-01-02 00:03:05"],
-                [3, "end", "end", "2022-01-02 00:03:05"],
-            ],
-            columns=["user_id", "event", "event_type", "timestamp"],
+    _source_df = pd.DataFrame(
+        [
+            [1, "start", "start", "2022-01-01 00:01:00"],
+            [1, "event1", "raw", "2022-01-01 00:01:00"],
+            [1, "event2", "raw", "2022-01-01 00:01:02"],
+            [1, "event1", "raw", "2022-01-01 00:02:00"],
+            [1, "event1", "raw", "2022-01-01 00:03:00"],
+            [1, "event1", "synthetic", "2022-01-01 00:03:00"],
+            [1, "session_start", "session_start", "2022-01-01 00:03:30"],
+            [1, "event3", "raw", "2022-01-01 00:03:30"],
+            [1, "event3_synthetic", "synthetic", "2022-01-01 00:03:30"],
+            [1, "event1", "raw", "2022-01-01 00:04:00"],
+            [1, "event3", "raw", "2022-01-01 00:04:30"],
+            [1, "event1", "raw", "2022-01-01 00:05:00"],
+            [2, "event1", "raw", "2022-01-02 00:00:00"],
+            [2, "event3", "raw", "2022-01-02 00:00:05"],
+            [2, "event2", "raw", "2022-01-02 00:01:05"],
+            [2, "end", "end", "2022-01-02 00:01:05"],
+            [3, "event1", "raw", "2022-01-02 00:01:10"],
+            [3, "event1", "raw", "2022-01-02 00:02:05"],
+            [3, "event4", "raw", "2022-01-02 00:03:05"],
+            [3, "end", "end", "2022-01-02 00:03:05"],
+        ],
+        columns=["user_id", "event", "event_type", "timestamp"],
+    )
+    _raw_data_schema = RawDataSchema(
+        user_id="user_id",
+        event_name="event",
+        event_type="event_type",
+        event_timestamp="timestamp",
+    )
+
+    def _apply(self, params: DeleteUsersByPathLengthParams) -> pd.DataFrame:
+        original, actual = apply_processor(
+            DeleteUsersByPathLength(params),
+            self._source_df,
+            raw_data_schema=self._raw_data_schema,
         )
+        return actual
 
-        correct_result_columns = ["user_id", "event_name", "event_type", "event_timestamp", "_deleted"]
-
-        correct_result = pd.DataFrame(
+    def test_delete_users_by_path_length_apply__by_event_4(self):
+        actual = self._apply(DeleteUsersByPathLengthParams(
+            events_num=4,
+        ))
+        expected = pd.DataFrame(
             [
                 [2, "event1", "raw", "2022-01-02 00:00:00", True],
                 [2, "event3", "raw", "2022-01-02 00:00:05", True],
@@ -52,108 +69,72 @@ class TestDeleteUsersByPathLength:
                 [3, "event4", "raw", "2022-01-02 00:03:05", True],
                 [3, "end", "end", "2022-01-02 00:03:05", True],
             ],
-            columns=correct_result_columns,
+            columns=["user_id", "event_name", "event_type", "event_timestamp", "_deleted"],
         )
-
-        source = Eventstream(
-            raw_data_schema=RawDataSchema(
-                event_name="event", event_timestamp="timestamp", user_id="user_id", event_type="event_type"
-            ),
-            raw_data=source_df,
-            schema=EventstreamSchema(),
-        )
-        events = DeleteUsersByPathLength(params=DeleteUsersByPathLengthParams(events_num=4))
-
-        result = events.apply(source)
-        result_df = result.to_dataframe(show_deleted=True)[correct_result_columns].reset_index(drop=True)
-
-        assert result_df.compare(correct_result).shape == (0, 0)
+        assert actual[expected.columns].compare(expected).shape == (0, 0)
 
     def test_delete_users_by_path_length_apply__by_cutoff(self):
-        source_df = pd.DataFrame(
-            [
-                [1, "start", "start", "2022-01-01 00:01:00"],
-                [1, "event1", "raw", "2022-01-01 00:01:00"],
-                [1, "event2", "raw", "2022-01-01 00:01:02"],
-                [1, "event1", "raw", "2022-01-01 00:02:00"],
-                [1, "event1", "raw", "2022-01-01 00:03:00"],
-                [1, "event1", "synthetic", "2022-01-01 00:03:00"],
-                [1, "session_start", "session_start", "2022-01-01 00:03:30"],
-                [1, "event3", "raw", "2022-01-01 00:03:30"],
-                [1, "event3_synthetic", "synthetic", "2022-01-01 00:03:30"],
-                [1, "event1", "raw", "2022-01-01 00:04:00"],
-                [1, "event3", "raw", "2022-01-01 00:04:30"],
-                [1, "event1", "raw", "2022-01-01 00:05:00"],
-                [2, "event1", "raw", "2022-01-02 00:00:00"],
-                [2, "event3", "raw", "2022-01-02 00:00:05"],
-                [2, "event2", "raw", "2022-01-02 00:01:05"],
-                [2, "end", "end", "2022-01-02 00:01:05"],
-                [3, "event1", "raw", "2022-01-02 00:01:10"],
-                [3, "event1", "raw", "2022-01-02 00:02:05"],
-                [3, "event4", "raw", "2022-01-02 00:03:05"],
-                [3, "end", "end", "2022-01-02 00:03:05"],
-            ],
-            columns=["user_id", "event", "event_type", "timestamp"],
-        )
-
-        correct_result_columns = ["user_id", "event_name", "event_type", "event_timestamp", "_deleted"]
-
-        correct_result = pd.DataFrame(
+        actual = self._apply(DeleteUsersByPathLengthParams(
+            cutoff=(1.5, "m"),
+        ))
+        expected = pd.DataFrame(
             [
                 [2, "event1", "raw", "2022-01-02 00:00:00", True],
                 [2, "event3", "raw", "2022-01-02 00:00:05", True],
                 [2, "event2", "raw", "2022-01-02 00:01:05", True],
                 [2, "end", "end", "2022-01-02 00:01:05", True],
             ],
-            columns=correct_result_columns,
+            columns=["user_id", "event_name", "event_type", "event_timestamp", "_deleted"],
         )
-
-        source = Eventstream(
-            raw_data_schema=RawDataSchema(
-                event_name="event", event_timestamp="timestamp", user_id="user_id", event_type="event_type"
-            ),
-            raw_data=source_df,
-            schema=EventstreamSchema(),
-        )
-        events = DeleteUsersByPathLength(params=DeleteUsersByPathLengthParams(cutoff=(1.5, "m")))
-
-        result = events.apply(source)
-        result_df = result.to_dataframe(show_deleted=True)[correct_result_columns].reset_index(drop=True)
-
-        assert result_df.compare(correct_result).shape == (0, 0)
+        assert actual[expected.columns].compare(expected).shape == (0, 0)
 
 
 class TestDeleteUsersByPathLengthGraph:
+    _source_df = pd.DataFrame(
+        [
+            [1, "start", "start", "2022-01-01 00:01:00"],
+            [1, "event1", "raw", "2022-01-01 00:01:00"],
+            [1, "event2", "raw", "2022-01-01 00:01:02"],
+            [1, "event1", "raw", "2022-01-01 00:02:00"],
+            [1, "event1", "raw", "2022-01-01 00:03:00"],
+            [1, "event1", "synthetic", "2022-01-01 00:03:00"],
+            [1, "session_start", "session_start", "2022-01-01 00:03:30"],
+            [1, "event3", "raw", "2022-01-01 00:03:30"],
+            [1, "event3_synthetic", "synthetic", "2022-01-01 00:03:30"],
+            [1, "event1", "raw", "2022-01-01 00:04:00"],
+            [1, "event3", "raw", "2022-01-01 00:04:30"],
+            [1, "event1", "raw", "2022-01-01 00:05:00"],
+            [2, "event1", "raw", "2022-01-02 00:00:00"],
+            [2, "event3", "raw", "2022-01-02 00:00:05"],
+            [2, "event2", "raw", "2022-01-02 00:01:05"],
+            [2, "end", "end", "2022-01-02 00:01:05"],
+            [3, "event1", "raw", "2022-01-02 00:01:10"],
+            [3, "event1", "raw", "2022-01-02 00:02:05"],
+            [3, "event4", "raw", "2022-01-02 00:03:05"],
+            [3, "end", "end", "2022-01-02 00:03:05"],
+        ],
+        columns=["user_id", "event", "event_type", "timestamp"],
+    )
+    _raw_data_schema = RawDataSchema(
+        user_id="user_id",
+        event_name="event",
+        event_type="event_type",
+        event_timestamp="timestamp",
+    )
+
+    def _apply(self, params: DeleteUsersByPathLengthParams) -> pd.DataFrame:
+        original, actual = apply_processor_with_graph(
+            DeleteUsersByPathLength(params),
+            self._source_df,
+            raw_data_schema=self._raw_data_schema,
+        )
+        return actual
+
     def test_delete_users_by_path_length_graph__by_event_4(self):
-        source_df = pd.DataFrame(
-            [
-                [1, "start", "start", "2022-01-01 00:01:00"],
-                [1, "event1", "raw", "2022-01-01 00:01:00"],
-                [1, "event2", "raw", "2022-01-01 00:01:02"],
-                [1, "event1", "raw", "2022-01-01 00:02:00"],
-                [1, "event1", "raw", "2022-01-01 00:03:00"],
-                [1, "event1", "synthetic", "2022-01-01 00:03:00"],
-                [1, "session_start", "session_start", "2022-01-01 00:03:30"],
-                [1, "event3", "raw", "2022-01-01 00:03:30"],
-                [1, "event3_synthetic", "synthetic", "2022-01-01 00:03:30"],
-                [1, "event1", "raw", "2022-01-01 00:04:00"],
-                [1, "event3", "raw", "2022-01-01 00:04:30"],
-                [1, "event1", "raw", "2022-01-01 00:05:00"],
-                [2, "event1", "raw", "2022-01-02 00:00:00"],
-                [2, "event3", "raw", "2022-01-02 00:00:05"],
-                [2, "event2", "raw", "2022-01-02 00:01:05"],
-                [2, "end", "end", "2022-01-02 00:01:05"],
-                [3, "event1", "raw", "2022-01-02 00:01:10"],
-                [3, "event1", "raw", "2022-01-02 00:02:05"],
-                [3, "event4", "raw", "2022-01-02 00:03:05"],
-                [3, "end", "end", "2022-01-02 00:03:05"],
-            ],
-            columns=["user_id", "event", "event_type", "timestamp"],
-        )
-
-        correct_result_columns = ["user_id", "event_name", "event_type", "event_timestamp"]
-
-        correct_result = pd.DataFrame(
+        actual = self._apply(DeleteUsersByPathLengthParams(
+            events_num=4,
+        ))
+        expected = pd.DataFrame(
             [
                 [1, "start", "start", "2022-01-01 00:01:00"],
                 [1, "event1", "raw", "2022-01-01 00:01:00"],
@@ -168,55 +149,15 @@ class TestDeleteUsersByPathLengthGraph:
                 [1, "event3", "raw", "2022-01-01 00:04:30"],
                 [1, "event1", "raw", "2022-01-01 00:05:00"],
             ],
-            columns=correct_result_columns,
+            columns=["user_id", "event_name", "event_type", "event_timestamp"],
         )
-
-        source = Eventstream(
-            raw_data_schema=RawDataSchema(
-                event_name="event", event_timestamp="timestamp", user_id="user_id", event_type="event_type"
-            ),
-            raw_data=source_df,
-            schema=EventstreamSchema(),
-        )
-        events = EventsNode(DeleteUsersByPathLength(params=DeleteUsersByPathLengthParams(events_num=4)))
-        graph = PGraph(source_stream=source)
-        graph.add_node(node=events, parents=[graph.root])
-
-        result = graph.combine(node=events)
-        result_df = result.to_dataframe()[correct_result_columns].reset_index(drop=True)
-
-        assert result_df.compare(correct_result).shape == (0, 0)
+        assert actual[expected.columns].compare(expected).shape == (0, 0)
 
     def test_delete_users_by_path_length_graph__by_cutoff(self):
-        source_df = pd.DataFrame(
-            [
-                [1, "start", "start", "2022-01-01 00:01:00"],
-                [1, "event1", "raw", "2022-01-01 00:01:00"],
-                [1, "event2", "raw", "2022-01-01 00:01:02"],
-                [1, "event1", "raw", "2022-01-01 00:02:00"],
-                [1, "event1", "raw", "2022-01-01 00:03:00"],
-                [1, "event1", "synthetic", "2022-01-01 00:03:00"],
-                [1, "session_start", "session_start", "2022-01-01 00:03:30"],
-                [1, "event3", "raw", "2022-01-01 00:03:30"],
-                [1, "event3_synthetic", "synthetic", "2022-01-01 00:03:30"],
-                [1, "event1", "raw", "2022-01-01 00:04:00"],
-                [1, "event3", "raw", "2022-01-01 00:04:30"],
-                [1, "event1", "raw", "2022-01-01 00:05:00"],
-                [2, "event1", "raw", "2022-01-02 00:00:00"],
-                [2, "event3", "raw", "2022-01-02 00:00:05"],
-                [2, "event2", "raw", "2022-01-02 00:01:05"],
-                [2, "end", "end", "2022-01-02 00:01:05"],
-                [3, "event1", "raw", "2022-01-02 00:01:10"],
-                [3, "event1", "raw", "2022-01-02 00:02:05"],
-                [3, "event4", "raw", "2022-01-02 00:03:05"],
-                [3, "end", "end", "2022-01-02 00:03:05"],
-            ],
-            columns=["user_id", "event", "event_type", "timestamp"],
-        )
-
-        correct_result_columns = ["user_id", "event_name", "event_type", "event_timestamp"]
-
-        correct_result = pd.DataFrame(
+        actual = self._apply(DeleteUsersByPathLengthParams(
+            cutoff=(1.5, "m"),
+        ))
+        expected = pd.DataFrame(
             [
                 [1, "start", "start", "2022-01-01 00:01:00"],
                 [1, "event1", "raw", "2022-01-01 00:01:00"],
@@ -235,21 +176,6 @@ class TestDeleteUsersByPathLengthGraph:
                 [3, "event4", "raw", "2022-01-02 00:03:05"],
                 [3, "end", "end", "2022-01-02 00:03:05"],
             ],
-            columns=correct_result_columns,
+            columns=["user_id", "event_name", "event_type", "event_timestamp"],
         )
-
-        source = Eventstream(
-            raw_data_schema=RawDataSchema(
-                event_name="event", event_timestamp="timestamp", user_id="user_id", event_type="event_type"
-            ),
-            raw_data=source_df,
-            schema=EventstreamSchema(),
-        )
-        events = EventsNode(DeleteUsersByPathLength(params=DeleteUsersByPathLengthParams(cutoff=(1.5, "m"))))
-        graph = PGraph(source_stream=source)
-        graph.add_node(node=events, parents=[graph.root])
-
-        result = graph.combine(node=events)
-        result_df = result.to_dataframe()[correct_result_columns].reset_index(drop=True)
-
-        assert result_df.compare(correct_result).shape == (0, 0)
+        assert actual[expected.columns].compare(expected).shape == (0, 0)

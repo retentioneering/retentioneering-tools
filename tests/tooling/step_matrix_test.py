4ee434b44be6c94a2ce964ef23b2db11ea9e1cf4
v.kukushkin <kukushkin.v@gmail.com>
2022-11-24 15:16:57 +0100

    Moved fixtures for step_matrix_test to a separate file

--- tests/tooling/step_matrix_test.py
+++ tests/tooling/step_matrix_test.py
@@ -1,18 +1,9 @@
 import os
 
 import pandas as pd
-import pytest
 
-from src import datasets
-from src.data_processors_lib.rete import (
-    FilterEvents,
-    FilterEventsParams,
-    StartEndEvents,
-    StartEndEventsParams,
-)
-from src.eventstream import Eventstream, EventstreamSchema, RawDataSchema
-from src.graph.p_graph import EventsNode, PGraph
 from src.tooling.step_matrix import StepMatrix
+from tests.tooling.fixtures.step_matrix import stream_simple, stream_simple_shop
 
 FLOAT_PRECISION = 6
 
@@ -24,58 +15,6 @@ def read_test_data(filename):
     df = pd.read_csv(filepath, index_col=0).round(FLOAT_PRECISION)
     df.columns = df.columns.astype(int)
     return df
-
-
-@pytest.fixture
-def stream():
-    def remove_start(df, schema):
-        return df["event_name"] != "path_start"
-
-    test_stream = datasets.load_simple_shop()
-    graph = PGraph(source_stream=test_stream)
-    node1 = EventsNode(StartEndEvents(params=StartEndEventsParams(**{})))
-    node2 = EventsNode(FilterEvents(params=FilterEventsParams(filter=remove_start)))
-
-    graph.add_node(node=node1, parents=[graph.root])
-    graph.add_node(node=node2, parents=[node1])
-
-    stream = graph.combine(node=node2)
-    return stream
-
-
-@pytest.fixture
-def stream_simple():
-    source_df = pd.DataFrame(
-        [
-            [1, "event1", "2022-01-01 00:01:00"],
-            [1, "event2", "2022-01-01 00:01:02"],
-            [1, "event1", "2022-01-01 00:02:00"],
-            [1, "event1", "2022-01-01 00:03:00"],
-            [1, "event1", "2022-01-01 00:03:00"],
-            [1, "event3", "2022-01-01 00:03:30"],
-            [1, "event1", "2022-01-01 00:04:00"],
-            [1, "event3", "2022-01-01 00:04:30"],
-            [1, "event1", "2022-01-01 00:05:00"],
-            [2, "event1", "2022-01-02 00:00:00"],
-            [2, "event2", "2022-01-02 00:00:05"],
-            [2, "event2", "2022-01-02 00:01:05"],
-            [3, "event1", "2022-01-02 00:01:10"],
-            [3, "event1", "2022-01-02 00:02:05"],
-            [3, "event4", "2022-01-02 00:03:05"],
-            [4, "event1", "2022-01-02 00:01:10"],
-            [4, "event1", "2022-01-02 00:02:05"],
-            [4, "event1", "2022-01-02 00:03:05"],
-        ],
-        columns=["user_id", "event", "timestamp"],
-    )
-
-    source_stream = Eventstream(
-        raw_data=source_df,
-        raw_data_schema=RawDataSchema(event_name="event", event_timestamp="timestamp", user_id="user_id"),
-        schema=EventstreamSchema(),
-    )
-
-    return source_stream
 
 
 def run_test(stream, filename, **kwargs):
@@ -127,11 +66,11 @@ class TestStepMatrix:
         )
         assert result.compare(correct_result).shape == (0, 0)
 
-    def test_step_matrix__basic(self, stream):
-        assert run_test(stream, "01_basic.csv")
+    def test_step_matrix__basic(self, stream_simple_shop):
+        assert run_test(stream_simple_shop, "01_basic.csv")
 
-    def test_step_matrix__one_step(self, stream):
-        assert run_test(stream, "02_one_step.csv", max_steps=1)
+    def test_step_matrix__one_step(self, stream_simple_shop):
+        assert run_test(stream_simple_shop, "02_one_step.csv", max_steps=1)
 
-    def test_step_matrix__100_steps(self, stream):
-        assert run_test(stream, "03_100_steps.csv", max_steps=100, precision=3)
+    def test_step_matrix__100_steps(self, stream_simple_shop):
+        assert run_test(stream_simple_shop, "03_100_steps.csv", max_steps=100, precision=3)

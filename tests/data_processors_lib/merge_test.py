cd569b5ba2f22f6792c1514fd715cb78e119cf50
Vladimir Makhanov <makhanov.vladimir@gmail.com>
2022-12-05 23:01:04 +0500

    update with master

--- tests/data_processors_lib/merge_test.py
+++ tests/data_processors_lib/merge_test.py
@@ -10,30 +10,25 @@ class TestMerge:
     def test__merge__dataprocessor(self) -> None:
         source_df = pd.DataFrame(
             [
-                {"event_name": "eventA", "event_timestamp": "2021-10-26 12:00", "user_id": "1"},
-                {"event_name": "eventA", "event_timestamp": "2021-10-26 12:02", "user_id": "1"},
-                {"event_name": "eventB", "event_timestamp": "2021-10-26 12:03", "user_id": "1"},
-                {"event_name": "eventB", "event_timestamp": "2021-10-26 12:03", "user_id": "1"},
-                {"event_name": "eventC", "event_timestamp": "2021-10-26 12:04", "user_id": "2"},
-                {"event_name": "eventC", "event_timestamp": "2021-10-26 12:05", "user_id": "1"},
+                {"event": "eventA", "timestamp": "2021-10-26 12:00", "user_id": "1"},
+                {"event": "eventA", "timestamp": "2021-10-26 12:02", "user_id": "1"},
+                {"event": "eventB", "timestamp": "2021-10-26 12:03", "user_id": "1"},
+                {"event": "eventB", "timestamp": "2021-10-26 12:03", "user_id": "1"},
+                {"event": "eventC", "timestamp": "2021-10-26 12:04", "user_id": "2"},
+                {"event": "eventC", "timestamp": "2021-10-26 12:05", "user_id": "1"},
             ]
         )
 
         expected = pd.DataFrame(
             [
-                {"event_name": "some_group", "event_timestamp": "2021-10-26 12:00", "user_id": "1"},
-                {"event_name": "some_group", "event_timestamp": "2021-10-26 12:02", "user_id": "1"},
-                {"event_name": "some_group", "event_timestamp": "2021-10-26 12:03", "user_id": "1"},
-                {"event_name": "some_group", "event_timestamp": "2021-10-26 12:03", "user_id": "1"},
+                {"event": "some_group", "timestamp": "2021-10-26 12:00", "user_id": "1"},
+                {"event": "some_group", "timestamp": "2021-10-26 12:02", "user_id": "1"},
+                {"event": "some_group", "timestamp": "2021-10-26 12:03", "user_id": "1"},
+                {"event": "some_group", "timestamp": "2021-10-26 12:03", "user_id": "1"},
             ]
         )
 
-        source = Eventstream(
-            raw_data_schema=RawDataSchema(
-                event_name="event_name", event_timestamp="event_timestamp", user_id="user_id"
-            ),
-            raw_data=source_df,
-        )
+        source = Eventstream(source_df)
 
         params = MergeParams(rules=self.rules)
         processor = MergeProcessor(params=params)
@@ -43,32 +38,27 @@ class TestMerge:
     def test__merge__helper(self):
         source_df = pd.DataFrame(
             [
-                {"event_name": "eventA", "event_timestamp": "2021-10-26 12:00", "user_id": "1"},
-                {"event_name": "eventA", "event_timestamp": "2021-10-26 12:02", "user_id": "1"},
-                {"event_name": "eventB", "event_timestamp": "2021-10-26 12:03", "user_id": "1"},
-                {"event_name": "eventB", "event_timestamp": "2021-10-26 12:03", "user_id": "1"},
-                {"event_name": "eventC", "event_timestamp": "2021-10-26 12:04", "user_id": "2"},
-                {"event_name": "eventC", "event_timestamp": "2021-10-26 12:05", "user_id": "1"},
+                {"event": "eventA", "timestamp": "2021-10-26 12:00", "user_id": "1"},
+                {"event": "eventA", "timestamp": "2021-10-26 12:02", "user_id": "1"},
+                {"event": "eventB", "timestamp": "2021-10-26 12:03", "user_id": "1"},
+                {"event": "eventB", "timestamp": "2021-10-26 12:03", "user_id": "1"},
+                {"event": "eventC", "timestamp": "2021-10-26 12:04", "user_id": "2"},
+                {"event": "eventC", "timestamp": "2021-10-26 12:05", "user_id": "1"},
             ]
         )
 
         expected = pd.DataFrame(
             [
-                {"event_name": "some_group", "event_timestamp": "2021-10-26 12:00", "user_id": "1"},
-                {"event_name": "some_group", "event_timestamp": "2021-10-26 12:02", "user_id": "1"},
-                {"event_name": "some_group", "event_timestamp": "2021-10-26 12:03", "user_id": "1"},
-                {"event_name": "some_group", "event_timestamp": "2021-10-26 12:03", "user_id": "1"},
-                {"event_name": "eventC", "event_timestamp": "2021-10-26 12:04", "user_id": "2"},
-                {"event_name": "eventC", "event_timestamp": "2021-10-26 12:05", "user_id": "1"},
+                {"event": "some_group", "timestamp": "2021-10-26 12:00", "user_id": "1"},
+                {"event": "some_group", "timestamp": "2021-10-26 12:02", "user_id": "1"},
+                {"event": "some_group", "timestamp": "2021-10-26 12:03", "user_id": "1"},
+                {"event": "some_group", "timestamp": "2021-10-26 12:03", "user_id": "1"},
+                {"event": "eventC", "timestamp": "2021-10-26 12:04", "user_id": "2"},
+                {"event": "eventC", "timestamp": "2021-10-26 12:05", "user_id": "1"},
             ]
         )
 
-        source = Eventstream(
-            raw_data_schema=RawDataSchema(
-                event_name="event_name", event_timestamp="event_timestamp", user_id="user_id"
-            ),
-            raw_data=source_df,
-        )
+        source = Eventstream(source_df)
 
         actual = source.merge(rules=self.rules)
         result_df = actual.to_dataframe()[expected.columns].reset_index(drop=True)

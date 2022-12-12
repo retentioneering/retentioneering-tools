1ded5152ce85670606f89006e5e260fbe6f71d0c
Julia Ostanina <j.ostanina@retentioneering.com>
2022-12-12 16:34:39 +0100

    after rewiew #2

--- tests/tooling/cohorts_test.py
+++ tests/tooling/cohorts_test.py
@@ -5,7 +5,18 @@ import pytest
 from pydantic import ValidationError
 
 from src.tooling.cohorts import Cohorts
-from tests.tooling.fixtures.cohorts import test_stream
+from tests.tooling.fixtures.cohorts_corr import (
+    MM_corr_corr,
+    matrix_M2M_corr,
+    matrix_matrix_D1M_corr,
+    matrix_matrix_D4W_corr,
+    matrix_matrix_D30D_corr,
+    matrix_matrix_matrix_avg_corr,
+    matrix_matrix_matrix_cut_corr,
+    matrix_matrix_W4W_corr,
+    matrix_matrix_W30D_corr,
+)
+from tests.tooling.fixtures.cohorts_input import test_stream
 
 
 def run_test(stream, correct_res, **kwargs):
@@ -17,80 +28,37 @@ def run_test(stream, correct_res, **kwargs):
 
 
 class TestCohorts:
-    def test_cohorts__matrix_MM(self, test_stream):
-        correct_res = pd.DataFrame(
-            [
-                [1.0, 1.0, 1.0, -999.0, 0.5],
-                [1.0, 1.0, -999.0, 0.25, -999.0],
-                [1.0, 0.67, 0.33, -999.00, -999.0],
-                [1.0, 1.0, -999.0, -999.00, -999.0],
-            ],
-            index=pd.Index(["2021-12", "2022-01", "2022-02", "2022-03"], name="CohortGroup"),
-            columns=pd.Index([0, 1, 2, 3, 4], dtype="int64", name="CohortPeriod"),
-        )
+    def test_cohorts__matrix_MM(self, test_stream, MM_corr_corr):
+        correct_res = MM_corr_corr
         assert run_test(test_stream, correct_res, cohort_start_unit="M", cohort_period=(1, "M"), average=False)
 
-    def test_cohorts__matrix_M2M(self, test_stream):
-        correct_res = pd.DataFrame(
-            [[1.0, 1.0, 0.33], [1.0, 0.5, -999.0]],
-            index=pd.Index(["2021-12", "2022-02"], name="CohortGroup"),
-            columns=pd.Index([0, 1, 2], dtype="int64", name="CohortPeriod"),
-        )
+    def test_cohorts__matrix_M2M(self, test_stream, matrix_M2M_corr):
+        correct_res = matrix_M2M_corr
         assert run_test(test_stream, correct_res, cohort_start_unit="M", cohort_period=(2, "M"), average=False)
 
-    def test_cohorts__matrix_D30D(self, test_stream):
-        correct_res = pd.DataFrame(
-            [[1.0, 1.0, -999.0, 0.33], [1.0, 0.67, 0.33, -999.0], [1.0, 1.0, -999.0, -999.0]],
-            index=pd.Index(["2021-12-28", "2022-01-27", "2022-02-26"], name="CohortGroup"),
-            columns=pd.Index([0, 1, 2, 3], dtype="int64", name="CohortPeriod"),
-        )
+    def test_cohorts__matrix_D30D(self, test_stream, matrix_matrix_D30D_corr):
+        correct_res = matrix_matrix_D30D_corr
         assert run_test(test_stream, correct_res, cohort_start_unit="D", cohort_period=(30, "D"), average=False)
 
-    def test_cohorts__matrix_W4W(self, test_stream):
-        correct_res = pd.DataFrame(
-            [[1.0, 1.0, -999.0, 0.33], [1.0, 0.6667, 0.33, -999.0], [1.0, 1.0, -999.0, -999.0]],
-            index=pd.Index(["2021-12-27", "2022-01-24", "2022-02-21"], name="CohortGroup"),
-            columns=pd.Index([0, 1, 2, 3], dtype="int64", name="CohortPeriod"),
-        )
+    def test_cohorts__matrix_W4W(self, test_stream, matrix_matrix_W4W_corr):
+        correct_res = matrix_matrix_W4W_corr
         assert run_test(test_stream, correct_res, cohort_start_unit="W", cohort_period=(4, "W"), average=False)
 
-    def test_cohorts__matrix_D4W(self, test_stream):
-        correct_res = pd.DataFrame(
-            [[1.0, 1.0, -999.0, 0.3333], [1.0, 0.6667, 0.3333, -999.0], [1.0, 1.0, -999.0, -999.0]],
-            index=pd.Index(["2021-12-28", "2022-01-25", "2022-02-22"], name="CohortGroup"),
-            columns=pd.Index([0, 1, 2, 3], dtype="int64", name="CohortPeriod"),
-        )
+    def test_cohorts__matrix_D4W(self, test_stream, matrix_matrix_D4W_corr):
+        correct_res = matrix_matrix_D4W_corr
         assert run_test(test_stream, correct_res, cohort_start_unit="D", cohort_period=(4, "W"), average=False)
 
-    def test_cohorts__matrix_W30D(self, test_stream):
-        correct_res = pd.DataFrame(
-            [[1.0, 1.0, -999.0, 0.3333], [1.0, 0.6667, 0.3333, -999.0], [1.0, 1.0, -999.0, -999.0]],
-            index=pd.Index(["2021-12-27", "2022-01-26", "2022-02-25"], name="CohortGroup"),
-            columns=pd.Index([0, 1, 2, 3], dtype="int64", name="CohortPeriod"),
-        )
+    def test_cohorts__matrix_W30D(self, test_stream, matrix_matrix_W30D_corr):
+        correct_res = matrix_matrix_W30D_corr
         assert run_test(test_stream, correct_res, cohort_start_unit="W", cohort_period=(30, "D"), average=False)
 
-    def test_cohorts__matrix_D1M(self, test_stream):
-        correct_res = pd.DataFrame(
-            [[1.0, 1.0, -999.0, 0.33], [1.0, 0.6667, 0.33, -999.0], [1.0, 1.0, -999.0, -999.0]],
-            index=pd.Index(["2021-12-27", "2022-01-24", "2022-02-21"], name="CohortGroup"),
-            columns=pd.Index([0, 1, 2, 3], dtype="int64", name="CohortPeriod"),
-        )
+    def test_cohorts__matrix_D1M(self, test_stream, matrix_matrix_D1M_corr):
+        correct_res = matrix_matrix_D1M_corr
         with pytest.raises(ValueError):
             p = run_test(test_stream, correct_res, cohort_start_unit="D", cohort_period=(1, "M"), average=False)
 
-    def test_cohorts__matrix_avg(self, test_stream):
-        correct_res = pd.DataFrame(
-            [
-                [1.0, 0.83, 0.17, -999.0, 0.33],
-                [1.0, -999, 0.5, 0.5, -999.0],
-                [1.0, 1.0, -999, -999.00, -999.0],
-                [1.0, 1.0, -999.0, -999.00, -999.0],
-                [1.0, 0.94, 0.33, 0.5, 0.33],
-            ],
-            index=pd.Index(["2021-12-28", "2022-01-18", "2022-02-08", "2022-03-01", "Average"], name="CohortGroup"),
-            columns=pd.Index([0, 1, 2, 3, 4], dtype="int64", name="CohortPeriod"),
-        )
+    def test_cohorts__matrix_avg(self, test_stream, matrix_matrix_matrix_avg_corr):
+        correct_res = matrix_matrix_matrix_avg_corr
         assert run_test(
             test_stream,
             correct_res,
@@ -102,12 +70,8 @@ class TestCohorts:
             cut_diagonal=0,
         )
 
-    def test_cohorts__matrix_cut(self, test_stream):
-        correct_res = pd.DataFrame(
-            [[1.0, 0.83]],
-            index=pd.Index(["2021-12-28"], name="CohortGroup"),
-            columns=pd.Index([0, 1], dtype="int64", name="CohortPeriod"),
-        )
+    def test_cohorts__matrix_cut(self, test_stream, matrix_matrix_matrix_cut_corr):
+        correct_res = matrix_matrix_matrix_cut_corr
         assert run_test(
             test_stream,
             correct_res,

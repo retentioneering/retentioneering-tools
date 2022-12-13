8b647fa49431b8cf6bb82922290cd57749bb5359
Julia Ostanina <j.ostanina@retentioneering.com>
2022-12-13 14:23:29 +0100

    cohorts tests after rewiew

--- tests/tooling/cohorts_test.py
+++ tests/tooling/cohorts_test.py
@@ -6,15 +6,15 @@ from pydantic import ValidationError
 
 from src.tooling.cohorts import Cohorts
 from tests.tooling.fixtures.cohorts_corr import (
-    MM_corr_corr,
+    MM_corr,
+    matrix_avg_corr,
+    matrix_cut_corr,
+    matrix_D1M_corr,
+    matrix_D4W_corr,
+    matrix_D30D_corr,
     matrix_M2M_corr,
-    matrix_matrix_D1M_corr,
-    matrix_matrix_D4W_corr,
-    matrix_matrix_D30D_corr,
-    matrix_matrix_matrix_avg_corr,
-    matrix_matrix_matrix_cut_corr,
-    matrix_matrix_W4W_corr,
-    matrix_matrix_W30D_corr,
+    matrix_W4W_corr,
+    matrix_W30D_corr,
 )
 from tests.tooling.fixtures.cohorts_input import test_stream
 
@@ -28,37 +28,37 @@ def run_test(stream, correct_res, **kwargs):
 
 
 class TestCohorts:
-    def test_cohorts__matrix_MM(self, test_stream, MM_corr_corr):
-        correct_res = MM_corr_corr
+    def test_cohorts__matrix_MM(self, test_stream, MM_corr):
+        correct_res = MM_corr
         assert run_test(test_stream, correct_res, cohort_start_unit="M", cohort_period=(1, "M"), average=False)
 
     def test_cohorts__matrix_M2M(self, test_stream, matrix_M2M_corr):
         correct_res = matrix_M2M_corr
         assert run_test(test_stream, correct_res, cohort_start_unit="M", cohort_period=(2, "M"), average=False)
 
-    def test_cohorts__matrix_D30D(self, test_stream, matrix_matrix_D30D_corr):
-        correct_res = matrix_matrix_D30D_corr
+    def test_cohorts__matrix_D30D(self, test_stream, matrix_D30D_corr):
+        correct_res = matrix_D30D_corr
         assert run_test(test_stream, correct_res, cohort_start_unit="D", cohort_period=(30, "D"), average=False)
 
-    def test_cohorts__matrix_W4W(self, test_stream, matrix_matrix_W4W_corr):
-        correct_res = matrix_matrix_W4W_corr
+    def test_cohorts__matrix_W4W(self, test_stream, matrix_W4W_corr):
+        correct_res = matrix_W4W_corr
         assert run_test(test_stream, correct_res, cohort_start_unit="W", cohort_period=(4, "W"), average=False)
 
-    def test_cohorts__matrix_D4W(self, test_stream, matrix_matrix_D4W_corr):
-        correct_res = matrix_matrix_D4W_corr
+    def test_cohorts__matrix_D4W(self, test_stream, matrix_D4W_corr):
+        correct_res = matrix_D4W_corr
         assert run_test(test_stream, correct_res, cohort_start_unit="D", cohort_period=(4, "W"), average=False)
 
-    def test_cohorts__matrix_W30D(self, test_stream, matrix_matrix_W30D_corr):
-        correct_res = matrix_matrix_W30D_corr
+    def test_cohorts__matrix_W30D(self, test_stream, matrix_W30D_corr):
+        correct_res = matrix_W30D_corr
         assert run_test(test_stream, correct_res, cohort_start_unit="W", cohort_period=(30, "D"), average=False)
 
-    def test_cohorts__matrix_D1M(self, test_stream, matrix_matrix_D1M_corr):
-        correct_res = matrix_matrix_D1M_corr
+    def test_cohorts__matrix_D1M(self, test_stream, matrix_D1M_corr):
+        correct_res = matrix_D1M_corr
         with pytest.raises(ValueError):
             p = run_test(test_stream, correct_res, cohort_start_unit="D", cohort_period=(1, "M"), average=False)
 
-    def test_cohorts__matrix_avg(self, test_stream, matrix_matrix_matrix_avg_corr):
-        correct_res = matrix_matrix_matrix_avg_corr
+    def test_cohorts__matrix_avg(self, test_stream, matrix_avg_corr):
+        correct_res = matrix_avg_corr
         assert run_test(
             test_stream,
             correct_res,
@@ -70,8 +70,8 @@ class TestCohorts:
             cut_diagonal=0,
         )
 
-    def test_cohorts__matrix_cut(self, test_stream, matrix_matrix_matrix_cut_corr):
-        correct_res = matrix_matrix_matrix_cut_corr
+    def test_cohorts__matrix_cut(self, test_stream, matrix_cut_corr):
+        correct_res = matrix_cut_corr
         assert run_test(
             test_stream,
             correct_res,

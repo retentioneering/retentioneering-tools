ea9ba3b056566643603fe5fd240e1205f1e7c2e4
Julia Ostanina <j.ostanina@retentioneering.com>
2022-12-22 19:12:07 +0100

    Added tests for Clusters

--- tests/tooling/clusters_test.py
+++ tests/tooling/clusters_test.py
@@ -4,8 +4,25 @@ import numpy as np
 import pandas as pd
 import pytest
 
+from src.eventstream import Eventstream, EventstreamSchema
 from src.tooling.clusters import Clusters
-from tests.tooling.fixtures.clusters import stream_simple_shop
+from tests.tooling.fixtures.clusters import (
+    custom_vector,
+    stream_simple_shop,
+    test_stream,
+)
+from tests.tooling.fixtures.clusters_corr import (
+    cluster_mapping_corr,
+    count_corr,
+    gmm_corr,
+    kmeans_corr,
+    markov_corr,
+    ngram_range_corr,
+    set_clusters_corr,
+    time_corr,
+    time_fraction_corr,
+    vector_corr,
+)
 
 
 class TestClusters:
@@ -53,3 +70,50 @@ class TestClusters:
             c.set_clusters(user_clusters)
         except Exception as e:
             pytest.fail("Runtime error in Clusters.set_clusters. " + str(e))
+
+    def test_clusters_vectorization__markov(self, test_stream, markov_corr):
+        correct_features = markov_corr
+        c = Clusters(eventstream=test_stream)
+        features = c.extract_features(feature_type="markov")
+        assert features.compare(correct_features).shape == (0, 0)
+
+    def test_clusters_vectorization__count(self, test_stream, count_corr):
+        correct_features = count_corr
+        c = Clusters(eventstream=test_stream)
+        features = c.extract_features(feature_type="count", ngram_range=(1, 1))
+        assert features.compare(correct_features).shape == (0, 0)
+
+    def test_clusters_method__kmeans_(self, test_stream, kmeans_corr):
+        correct_result = kmeans_corr
+        c = Clusters(eventstream=test_stream)
+        c.fit(method="kmeans", n_clusters=2, feature_type="tfidf", ngram_range=(1, 1))
+        result = c.user_clusters
+        assert sum(result == correct_result) == 4
+
+    def test_clusters_method__gmm(self, test_stream, gmm_corr):
+        correct_result = gmm_corr
+        c = Clusters(eventstream=test_stream)
+        c.fit(method="gmm", n_clusters=2, feature_type="tfidf", ngram_range=(1, 1))
+        result = c.user_clusters
+        assert sum(result != correct_result) == 0
+
+    def test_clusters__cluster_mapping(self, test_stream, cluster_mapping_corr):
+        correct_result = cluster_mapping_corr
+        c = Clusters(eventstream=test_stream)
+        c.fit(method="gmm", n_clusters=2, feature_type="tfidf", ngram_range=(1, 1))
+        result = c.cluster_mapping
+        assert result == correct_result
+
+    def test_clusters__ngram_range(self, test_stream, ngram_range_corr):
+        correct_features = ngram_range_corr
+        c = Clusters(eventstream=test_stream)
+        features = c.extract_features(feature_type="count", ngram_range=(3, 3))
+        assert features.compare(correct_features).shape == (0, 0)
+
+    def test_clusters__set_clusters(self, test_stream, set_clusters_corr):
+        correct_result = set_clusters_corr
+        c = Clusters(eventstream=test_stream)
+        user_clusters = pd.Series([1, 3, 0, 2])
+        c.set_clusters(user_clusters)
+        result = c.cluster_mapping
+        assert result == correct_result

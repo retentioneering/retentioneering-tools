bdf06e975e019f8433163f6a3ae9531405ca1443
Julia Ostanina <j.ostanina@retentioneering.com>
2023-01-09 15:04:44 +0100

    some fixes

--- tests/tooling/clusters_test.py
+++ tests/tooling/clusters_test.py
@@ -27,51 +27,6 @@ from tests.tooling.fixtures.clusters_corr import (
 
 
 class TestClusters:
-    def test_clusters__simple_shop_no_exceptions(self, stream_simple_shop):
-        c = Clusters(eventstream=stream_simple_shop)
-        try:
-            c.fit(method="kmeans", n_clusters=4, feature_type="tfidf", ngram_range=(1, 1))
-        except Exception as e:
-            pytest.fail("Runtime error in Clusters.fit. " + str(e))
-
-        try:
-            c.event_dist(cluster_id1=0)
-        except Exception as e:
-            pytest.fail("Runtime error in Clusters.event_dist. " + str(e))
-
-        try:
-            c.event_dist(cluster_id1=0, cluster_id2=1)
-        except Exception as e:
-            pytest.fail("Runtime error in Clusters.event_dist. " + str(e))
-
-        try:
-            c.plot()
-        except Exception as e:
-            pytest.fail("Runtime error in Clusters.plot. " + str(e))
-
-        try:
-            c.filter_cluster(cluster_id=0)
-        except Exception as e:
-            pytest.fail("Runtime error in Clusters.filter_cluster. " + str(e))
-
-        try:
-            c.extract_features(feature_type="count", ngram_range=(1, 2))
-        except Exception as e:
-            pytest.fail("Runtime error in Clusters.projection. " + str(e))
-
-        try:
-            c.projection()
-        except Exception as e:
-            pytest.fail("Runtime error in Clusters.projection. " + str(e))
-
-        try:
-            users = stream_simple_shop.to_dataframe()["user_id"].unique()
-            clusters = np.random.choice([0, 1, 2, 3], size=len(users))
-            user_clusters = pd.Series(clusters, index=users)
-            c.set_clusters(user_clusters)
-        except Exception as e:
-            pytest.fail("Runtime error in Clusters.set_clusters. " + str(e))
-
     def test_clusters_vectorization__markov(self, test_stream, markov_corr):
         correct_features = markov_corr
         c = Clusters(eventstream=test_stream)

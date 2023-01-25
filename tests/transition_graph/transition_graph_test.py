4013fe22920a69e35c0364b7853130b0773cbfe2
Julia Ostanina <j.ostanina@retentioneering.com>
2023-01-25 16:19:58 +0100

    transition_graph_tests in progress

---
+++ tests/transition_graph/transition_graph_test.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+import pytest
+
+from tests.transition_graph.fixtures.transition_input import test_stream
+
+
+class TestTransitionGraph:
+    def test_transition__simple(self, test_stream):
+        pass
+
+        return None

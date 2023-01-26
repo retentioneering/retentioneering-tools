852ed69172048cbd77e97fa13f169486a4a488c5
Julia Ostanina <j.ostanina@retentioneering.com>
2023-01-26 19:01:09 +0100

    positive-negative targets custom func testing

---
+++ tests/data_processors_lib/fixtures/positive_negative_target.py
@@ -0,0 +1,117 @@
+from __future__ import annotations
+
+import pandas as pd
+import pytest
+
+from retentioneering.eventstream import Eventstream, RawDataSchema
+
+
+@pytest.fixture
+def test_stream():
+    source_df = pd.DataFrame(
+        [
+            [1, "path_start", "path_start", "2022-01-01 00:01:00"],
+            [1, "event1", "raw", "2022-01-01 00:01:00"],
+            [1, "event2", "raw", "2022-01-01 00:01:02"],
+            [1, "event1", "raw", "2022-01-01 00:02:00"],
+            [1, "event1", "raw", "2022-01-01 00:03:00"],
+            [1, "event1", "synthetic", "2022-01-01 00:03:00"],
+            [1, "session_start", "session_start", "2022-01-01 00:03:30"],
+            [1, "event3", "raw", "2022-01-01 00:03:30"],
+            [1, "event1", "raw", "2022-01-01 00:04:00"],
+            [1, "event3", "raw", "2022-01-01 00:04:30"],
+            [1, "event1", "raw", "2022-01-01 00:05:00"],
+            [2, "event1", "raw", "2022-01-02 00:00:00"],
+            [2, "event3", "raw", "2022-01-02 00:00:05"],
+            [2, "event2", "raw", "2022-01-02 00:01:05"],
+            [2, "path_end", "path_end", "2022-01-02 00:01:05"],
+            [3, "event1", "raw", "2022-01-02 00:01:10"],
+            [3, "event1", "raw", "2022-01-02 00:02:05"],
+            [3, "event4", "raw", "2022-01-02 00:03:05"],
+            [3, "path_end", "path_end", "2022-01-02 00:03:05"],
+        ],
+        columns=["user_id", "event", "event_type", "timestamp"],
+    )
+    raw_data_schema = RawDataSchema(
+        user_id="user_id",
+        event_name="event",
+        event_type="event_type",
+        event_timestamp="timestamp",
+    )
+    evs = Eventstream(source_df, raw_data_schema=raw_data_schema)
+    return evs
+
+
+@pytest.fixture
+def positive_helper_custom_func_corr():
+    corr = pd.DataFrame(
+        [
+            [1, "path_start", "path_start", "2022-01-01 00:01:00"],
+            [1, "event1", "raw", "2022-01-01 00:01:00"],
+            [1, "positive_target_event1", "positive_target", "2022-01-01 00:01:00"],
+            [1, "event2", "raw", "2022-01-01 00:01:02"],
+            [1, "event1", "raw", "2022-01-01 00:02:00"],
+            [1, "event1", "raw", "2022-01-01 00:03:00"],
+            [1, "event1", "synthetic", "2022-01-01 00:03:00"],
+            [1, "session_start", "session_start", "2022-01-01 00:03:30"],
+            [1, "event3", "raw", "2022-01-01 00:03:30"],
+            [1, "event1", "raw", "2022-01-01 00:04:00"],
+            [1, "event3", "raw", "2022-01-01 00:04:30"],
+            [1, "event1", "raw", "2022-01-01 00:05:00"],
+            [1, "positive_target_event1", "positive_target", "2022-01-01 00:05:00"],
+            [2, "event1", "raw", "2022-01-02 00:00:00"],
+            [2, "positive_target_event1", "positive_target", "2022-01-02 00:00:00"],
+            [2, "positive_target_event1", "positive_target", "2022-01-02 00:00:00"],
+            [2, "event3", "raw", "2022-01-02 00:00:05"],
+            [2, "event2", "raw", "2022-01-02 00:01:05"],
+            [2, "path_end", "path_end", "2022-01-02 00:01:05"],
+            [3, "event1", "raw", "2022-01-02 00:01:10"],
+            [3, "positive_target_event1", "positive_target", "2022-01-02 00:01:10"],
+            [3, "event1", "raw", "2022-01-02 00:02:05"],
+            [3, "positive_target_event1", "positive_target", "2022-01-02 00:02:05"],
+            [3, "event4", "raw", "2022-01-02 00:03:05"],
+            [3, "path_end", "path_end", "2022-01-02 00:03:05"],
+        ],
+        columns=["user_id", "event", "event_type", "timestamp"],
+    )
+
+    corr["timestamp"] = pd.to_datetime(corr["timestamp"])
+    return corr
+
+
+@pytest.fixture
+def negative_helper_custom_func_corr():
+    corr = pd.DataFrame(
+        [
+            [1, "path_start", "path_start", "2022-01-01 00:01:00"],
+            [1, "event1", "raw", "2022-01-01 00:01:00"],
+            [1, "negative_target_event1", "negative_target", "2022-01-01 00:01:00"],
+            [1, "event2", "raw", "2022-01-01 00:01:02"],
+            [1, "event1", "raw", "2022-01-01 00:02:00"],
+            [1, "event1", "raw", "2022-01-01 00:03:00"],
+            [1, "event1", "synthetic", "2022-01-01 00:03:00"],
+            [1, "session_start", "session_start", "2022-01-01 00:03:30"],
+            [1, "event3", "raw", "2022-01-01 00:03:30"],
+            [1, "event1", "raw", "2022-01-01 00:04:00"],
+            [1, "event3", "raw", "2022-01-01 00:04:30"],
+            [1, "event1", "raw", "2022-01-01 00:05:00"],
+            [1, "negative_target_event1", "negative_target", "2022-01-01 00:05:00"],
+            [2, "event1", "raw", "2022-01-02 00:00:00"],
+            [2, "negative_target_event1", "negative_target", "2022-01-02 00:00:00"],
+            [2, "negative_target_event1", "negative_target", "2022-01-02 00:00:00"],
+            [2, "event3", "raw", "2022-01-02 00:00:05"],
+            [2, "event2", "raw", "2022-01-02 00:01:05"],
+            [2, "path_end", "path_end", "2022-01-02 00:01:05"],
+            [3, "event1", "raw", "2022-01-02 00:01:10"],
+            [3, "negative_target_event1", "negative_target", "2022-01-02 00:01:10"],
+            [3, "event1", "raw", "2022-01-02 00:02:05"],
+            [3, "negative_target_event1", "negative_target", "2022-01-02 00:02:05"],
+            [3, "event4", "raw", "2022-01-02 00:03:05"],
+            [3, "path_end", "path_end", "2022-01-02 00:03:05"],
+        ],
+        columns=["user_id", "event", "event_type", "timestamp"],
+    )
+
+    corr["timestamp"] = pd.to_datetime(corr["timestamp"])
+
+    return corr

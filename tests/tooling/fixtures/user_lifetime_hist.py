a8aed72994013574883e37a61672b0eb63e9658d
Daria Panina <paninad.a@gmail.com>
2023-02-12 23:06:43 +0100

    user_lifetime_hist class tests

--- tests/tooling/fixtures/user_lifetime_hist.py
+++ tests/tooling/fixtures/user_lifetime_hist.py
@@ -8,7 +8,7 @@ from retentioneering.eventstream import Eventstream
 
 def read_test_data(filename):
     current_dir = os.path.dirname(os.path.realpath(__file__))
-    test_data_dir = os.path.join(current_dir, "../../../datasets/eventstream/tooling/user_lifetime_hist")
+    test_data_dir = os.path.join(current_dir, "../../datasets/tooling/user_lifetime_hist")
     filepath = os.path.join(test_data_dir, filename)
     source_df = pd.read_csv(filepath)
     return source_df

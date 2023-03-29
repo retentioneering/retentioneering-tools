e8baeddc3fdc9cd03206a0ef58528ee010600887
Daria Panina <paninad.a@gmail.com>
2023-03-29 17:21:01 +0200

    tests change

---
+++ tests/eventstream/tooling/transition_matrix_test.py
@@ -0,0 +1,116 @@
+from __future__ import annotations
+
+import pandas as pd
+import pytest
+
+from retentioneering.eventstream.types import EventstreamType
+from tests.eventstream.tooling.fixtures.transition_matrix_corr import (
+    full_corr,
+    node_corr,
+    session_full_corr,
+    session_node_corr,
+    session_simple_corr,
+    simple_corr,
+    user_full_corr,
+    user_node_corr,
+    user_simple_corr,
+)
+from tests.eventstream.tooling.fixtures.transition_matrix_input import test_stream
+
+
+class TestEventstreamTransitionMatrix:
+    def test_eventstream_transition_matrix__simple(
+        self, test_stream: EventstreamType, simple_corr: pd.DataFrame
+    ) -> None:
+        result = test_stream.transition_matrix()
+        correct = simple_corr
+
+        assert pd.testing.assert_frame_equal(result, correct) is None
+
+    def test_eventstream_transition_matrix__norm_full(
+        self, test_stream: EventstreamType, full_corr: pd.DataFrame
+    ) -> None:
+        # tm = _TransitionMatrix(eventstream=test_stream)
+        # result = tm._values(norm_type="full")
+
+        result = test_stream.transition_matrix(norm_type="full")
+        correct = full_corr
+
+        assert pd.testing.assert_frame_equal(result, correct, atol=0.001) is None
+
+    def test_eventstream_transition_matrix__norm_node(
+        self, test_stream: EventstreamType, node_corr: pd.DataFrame
+    ) -> None:
+        # tm = _TransitionMatrix(eventstream=test_stream)
+        # result = tm._values(norm_type="node")
+
+        result = test_stream.transition_matrix(norm_type="node")
+        correct = node_corr
+
+        assert pd.testing.assert_frame_equal(result, correct, atol=0.001) is None
+
+    def test_eventstream_transition_matrix__session_simple(
+        self, test_stream: EventstreamType, session_simple_corr: pd.DataFrame
+    ) -> None:
+        # tm = _TransitionMatrix(eventstream=test_stream)
+        # result = tm._values(weight_col="session_id")
+
+        result = test_stream.transition_matrix(weight_col="session_id")
+        correct = session_simple_corr
+
+        assert pd.testing.assert_frame_equal(result, correct) is None
+
+    def test_eventstream_transition_matrix__session_full(
+        self, test_stream: EventstreamType, session_full_corr: pd.DataFrame
+    ) -> None:
+        # tm = _TransitionMatrix(eventstream=test_stream)
+        # result = tm._values(weight_col="session_id", norm_type="full")
+
+        result = test_stream.transition_matrix(weight_col="session_id", norm_type="full")
+        correct = session_full_corr
+
+        assert pd.testing.assert_frame_equal(result, correct, atol=0.001) is None
+
+    def test_eventstream_transition_matrix__session_node(
+        self, test_stream: EventstreamType, session_node_corr: pd.DataFrame
+    ) -> None:
+        # tm = _TransitionMatrix(eventstream=test_stream)
+        # result = tm._values(weight_col="session_id", norm_type="node")
+
+        result = test_stream.transition_matrix(weight_col="session_id", norm_type="node")
+        correct = session_node_corr
+
+        assert pd.testing.assert_frame_equal(result, correct, atol=0.001) is None
+
+    def test_eventstream_transition_matrix__user_simple(
+        self, test_stream: EventstreamType, user_simple_corr: pd.DataFrame
+    ) -> None:
+        # tm = _TransitionMatrix(eventstream=test_stream)
+        # result = tm._values(weight_col="user_id", norm_type=None)
+
+        result = test_stream.transition_matrix(weight_col="user_id", norm_type=None)
+        correct = user_simple_corr
+
+        assert pd.testing.assert_frame_equal(result, correct) is None
+
+    def test_eventstream_transition_matrix__users_full(
+        self, test_stream: EventstreamType, user_full_corr: pd.DataFrame
+    ) -> None:
+        # tm = _TransitionMatrix(eventstream=test_stream)
+        # result = tm._values(weight_col="user_id", norm_type="full")
+
+        result = test_stream.transition_matrix(weight_col="user_id", norm_type="full")
+        correct = user_full_corr
+
+        assert pd.testing.assert_frame_equal(result, correct, atol=0.001) is None
+
+    def test_eventstream_transition_matrix__users_node(
+        self, test_stream: EventstreamType, user_node_corr: pd.DataFrame
+    ) -> None:
+        # tm = _TransitionMatrix(eventstream=test_stream)
+        # result = tm._values(weight_col="user_id", norm_type="node")
+
+        result = test_stream.transition_matrix(weight_col="user_id", norm_type="node")
+        correct = user_node_corr
+
+        assert pd.testing.assert_frame_equal(result, correct, atol=0.001) is None
--- tests/tooling/describe_test.py
+++ tests/tooling/describe_test.py
@@ -3,20 +3,20 @@ from __future__ import annotations
 import pandas as pd
 
 from retentioneering.eventstream.types import EventstreamType
-from retentioneering.tooling.describe.describe import Describe
+from retentioneering.tooling.describe import _Describe
 from tests.tooling.fixtures.describe import test_stream
 from tests.tooling.fixtures.describe_corr import basic_corr, session_corr
 
 
 class TestDescribe:
     def test_describe__basic(self, test_stream: EventstreamType, basic_corr: pd.DataFrame):
-        de = Describe(test_stream)
-        result = de._describe()
+        de = _Describe(test_stream)
+        result = de._values()
         expected_df = basic_corr
         assert pd.testing.assert_frame_equal(result, expected_df) is None
 
     def test_describe__session(self, test_stream: EventstreamType, session_corr: pd.DataFrame):
-        de = Describe(test_stream.split_sessions(session_cutoff=(10, "m")))
-        result = de._describe()
+        de = _Describe(test_stream.split_sessions(session_cutoff=(10, "m")))
+        result = de._values()
         expected_df = session_corr
         assert pd.testing.assert_frame_equal(result, expected_df) is None

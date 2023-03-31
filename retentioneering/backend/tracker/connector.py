b15ecfbe8aa42b6602383d1318d6fb305c313be3
Vladimir Makhanov <makhanov.vladimir@gmail.com>
2023-03-31 15:55:02 +0500

    debug connector, fix params data

--- retentioneering/backend/tracker/connector.py
+++ retentioneering/backend/tracker/connector.py
@@ -22,13 +22,18 @@ class TrackerMainConnector(ConnectorProtocol):
     def __init__(self) -> None:
         super().__init__()
         self.url = "https://t.trsbf.com/endpoint/event"
-        self.source = "rete_tools"
+        self.source = "rete_transition_graph"
         self.session = requests.Session()
 
     def _post(self, data: dict) -> requests.Response:
+        data["source"] = self.source
+        data["params"] = str(data["params"])
         req = requests.Request("POST", self.url, data=json.dumps(data))
+        print(data)
         prepped = req.prepare()
-        return self.session.send(prepped)
+        response = self.session.send(prepped)
+        print(response.status_code)
+        return response
 
     def send_message(self, data: TrackingInfo) -> None:
         self._post(asdict(data))

56323d42d9c26ea3e01fc5a0d5abde57b97432bc
Julia Ostanina <j.ostanina@retentioneering.com>
2023-04-21 15:26:10 +0200

    some fixes

--- tests/tooling/transition_graph_test.py
+++ tests/tooling/transition_graph_test.py
@@ -74,9 +74,61 @@ class TestTransitionGraph:
         tg.plot(edges_threshold={"event_id": 3})
 
         assert tg.edges_thresholds == {"event_id": 3}
+        assert tg is not None
+        assert tg.nodes_weight_col == "event_id"
+        assert tg.edges_weight_col == "event_id"
+        assert tg.targets == {"negative": None, "positive": None, "source": None}
+        assert tg.edges_norm_type is None
+        assert tg.weight_cols == ["event_id", "user_id", "session_id"]
 
     def test_transition_graph__nodes_threshold(self, test_stream: EventstreamType) -> None:
         tg = TransitionGraph(eventstream=test_stream)
         tg.plot(nodes_threshold={"user_id": 2})
 
         assert tg.nodes_thresholds == {"user_id": 2}
+        assert tg is not None
+        assert tg.nodes_weight_col == "event_id"
+        assert tg.edges_weight_col == "event_id"
+        assert tg.targets == {"negative": None, "positive": None, "source": None}
+        assert tg.edges_norm_type is None
+        assert tg.weight_cols == ["event_id", "user_id", "session_id"]
+
+    def test_transition_graph__nodes_weight_col_session(self, test_stream: EventstreamType) -> None:
+        tg = TransitionGraph(eventstream=test_stream)
+        tg.plot(nodes_weight_col="session_id")
+
+        assert tg is not None
+        assert tg.nodes_weight_col == "session_id"
+        assert tg.edges_weight_col == "event_id"
+        assert tg.targets == {"negative": None, "positive": None, "source": None}
+        assert tg.edges_norm_type is None
+        assert tg.weight_cols == ["event_id", "user_id", "session_id"]
+
+    def test_transition_graph__edges_weight_col_session(self, test_stream: EventstreamType) -> None:
+        tg = TransitionGraph(eventstream=test_stream)
+        tg.plot(edges_weight_col="session_id")
+
+        assert tg is not None
+        assert tg.nodes_weight_col == "event_id"
+        assert tg.edges_weight_col == "session_id"
+        assert tg.targets == {"negative": None, "positive": None, "source": None}
+        assert tg.edges_norm_type is None
+        assert tg.weight_cols == ["event_id", "user_id", "session_id"]
+
+    def test_transition_graph__settings(self, test_stream: EventstreamType) -> None:
+        tg = TransitionGraph(eventstream=test_stream)
+        tg.plot(
+            show_percents=True,
+            edges_norm_type="full",
+            show_weights=False,
+            show_nodes_names=True,
+            show_all_edges_for_targets=False,
+            show_nodes_without_links=True,
+        )
+
+        assert tg is not None
+        assert tg.nodes_weight_col == "event_id"
+        assert tg.edges_weight_col == "event_id"
+        assert tg.targets == {"negative": None, "positive": None, "source": None}
+        assert tg.edges_norm_type == "full"
+        assert tg.weight_cols == ["event_id", "user_id", "session_id"]
